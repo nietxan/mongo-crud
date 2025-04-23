6eed1b93c4d8c6163c700d9e3b8b38fc942abc48
nietxan <niyetkhan.saduakassov@qcloudy.io>
2025-04-23 23:37:40 +0500

    fix: warning

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.use(express.json());
 app.use('/api/orders', ordersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI, {})
 .then(() => console.log('✅ MongoDB connected'))
 .catch(err => console.error('❌ MongoDB connection error:', err));
 
